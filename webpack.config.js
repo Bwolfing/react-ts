0a8eef8a50b2658b2cead989bee7b0601b5561cf
Billy Wolfington <wolfington98@gmail.com>
2018-05-12 18:53:03 -0700

    Webpack build process

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+
+const path = require("path");
+const tsconfigPaths = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "app.js",
+        path: path.join(__dirname, "dist")
+    },
+
+    mode: "development",
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [ ".ts", ".tsx", ".js", ".json" ],
+        plugins: [
+            new tsconfigPaths()
+        ]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                enforce: "pre",
+                test: /.js?$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+}
