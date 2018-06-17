fbf1fd9ca1404ca0792b6dea5f4b5cc342645283
William Wolfram <willwolfram18@gmail.com>
2018-06-16 19:28:01 -0700

    server returns data

--- server/index.js
+++ server/index.js
@@ -8,6 +8,31 @@ const dist = path.join(root, "dist");
 app.use(express.static(dist));
 app.use(express.static(path.join(root, "assets")));
 
+app.get("/api/todos", (request, response) => {
+    response.send([
+        {
+            id: 1,
+            completed: true,
+            text: "Read about React"
+        },
+        {
+            id: 2,
+            completed: true,
+            text: "Build with React"
+        },
+        {
+            id: 3,
+            completed: false,
+            text: "Read about Redux"
+        },
+        {
+            id: 4,
+            completed: false,
+            text: "Build with Redux"
+        },
+    ]);
+});
+
 app.get("*", (request, response) => {
     response.sendFile(path.join(root, "index.html"));
 });
