bb7e79ee77678e9e66612bc57968c7aa8e061e06
William Wolfram <willwolfram18@gmail.com>
2018-06-16 19:22:02 -0700

    async action created for fetching todos

--- src/state/actions/TodoListActions.ts
+++ src/state/actions/TodoListActions.ts
@@ -1,6 +1,8 @@
-import { Action } from "redux";
+import { fetch } from "cross-fetch";
+import { Action, Dispatch } from "redux";
 
-export type TodoListActionTypes = "Add Todo" | "Toggle Todo" | "Set Visibility";
+export type TodoListActionTypes = "Add Todo" | "Toggle Todo" | "Set Visibility" |
+    "Fetch Todos" | "Received Todos" | "Failed Todos Fetch";
 
 export const enum TodoListVisibility {
     All,
@@ -27,6 +29,20 @@ export interface TodoFilterAction extends TodoListAction {
     filter: TodoListVisibility;
 }
 
+export interface FetchTodosAction extends TodoListAction {
+    type: "Fetch Todos";
+}
+
+export interface RecievedTodosAction extends TodoListAction {
+    type: "Received Todos";
+    todos: TodoItem[];
+}
+
+export interface FailedTodosFetchAction extends TodoListAction {
+    type: "Failed Todos Fetch";
+    error: Error;
+}
+
 export function addTodo(text: string): AddTodoAction {
     return {
         text,
@@ -45,5 +61,36 @@ export function setFilter(filter: TodoListVisibility): TodoFilterAction {
     return {
         filter,
         type: "Set Visibility"
+    };
+}
+
+export function receivedTodos(todos: TodoItem[]): RecievedTodosAction {
+    return {
+        type: "Received Todos",
+        todos
+    };
+}
+
+export function failedTodosFetch(error: Error): FailedTodosFetchAction {
+    return {
+        type: "Failed Todos Fetch",
+        error
+    };
+}
+
+export function fetchTodos() {
+    return (dispatch: Dispatch) => {
+        dispatch({
+            type: "Fetch Todos"
+        });
+
+        return fetch("/api/todos")
+            .then(
+                response => <Promise<TodoItem[]>>response.json()
+            )
+            .then(
+                todos => dispatch(receivedTodos(todos)),
+                error => dispatch(failedTodosFetch(error))
+            );
     };
 }
