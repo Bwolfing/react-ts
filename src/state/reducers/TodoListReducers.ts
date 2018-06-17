bdb9cb254df9df98f3850bbda7acec49631f00ef
William Wolfram <willwolfram18@gmail.com>
2018-06-16 21:32:18 -0700

    Refreshing todo and rending fetched todos

--- src/state/reducers/TodoListReducers.ts
+++ src/state/reducers/TodoListReducers.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import { TodoListAction, ToggleTodoAction, AddTodoAction, TodoListVisibility, TodoFilterAction } from "@app/state/actions/TodoListActions";
+import { TodoListAction, ToggleTodoAction, AddTodoAction, TodoListVisibility, TodoFilterAction, FulfilledTodoFetchAction } from "@app/state/actions/TodoListActions";
 
 function todos(state: TodoItem[] = [], action: TodoListAction): TodoItem[] {
     switch (action.type) {
@@ -25,6 +25,8 @@ function todos(state: TodoItem[] = [], action: TodoListAction): TodoItem[] {
                     text: newTodo.text
                 }
             ];
+        case "Fetch Todos_FULFILLED":
+            return (<FulfilledTodoFetchAction>action).payload;
         default:
             return state;
     }
@@ -39,7 +41,19 @@ function filter(state: TodoListVisibility = TodoListVisibility.All, action: Todo
     }
 }
 
+function fetchingTodos(state: boolean, action: TodoListAction): boolean {
+    switch (action.type) {
+        case "Fetch Todos_PENDING":
+            return true;
+        case "Fetch Todos_FULFILLED":
+        case "Fetch Todos_REJECTED":
+        default:
+            return false;
+    }
+}
+
 export const todoApp = combineReducers({
     todos,
-    filter
+    filter,
+    fetchingTodos
 });
